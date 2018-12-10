3f1e35ee1f78399e6f532caafacff884a253e7c9
bevrivling <bev.rivling@gmail.com>
2018-12-10 21:02:01 +0000

    placeholders added to cards

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -4,10 +4,13 @@ const ArticleCard = ({ articleInfo }) => {
   console.log(articleInfo);
   return (
     <div className="article-card">
-      <h4>{articleInfo.title}</h4>
-      <p>Author: {articleInfo.author}</p>
-      <p>Date: {articleInfo.created_at}</p>
-      <p>Votes: {articleInfo.votes}</p>
+      <div className="placeholder" />
+      <div className="card-content">
+        <h4>{articleInfo.title}</h4>
+        <p>Author: {articleInfo.author}</p>
+        <p>Date: {articleInfo.created_at}</p>
+        <p>Votes: {articleInfo.votes}</p>
+      </div>
     </div>
   );
 };
